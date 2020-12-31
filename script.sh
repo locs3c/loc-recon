0a83ede88928947a977f07bbf3a7233e8d7f2d8d
Lucas Oliveira <lucasinfobarretos@gmail.com>
2020-12-31 14:28:15 -0300

    script.sh
    script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for host in $(cat /content/b3.txt);do
+echo "Realizando Analise do Ativo:"$host
+python3 script.py $host >> final.txt
+done
+sort final.txt >> resultado.txt
+echo "Processo Concluido! Arquivo salvo como resultado.txt"
+rm final.txt
