7496139afacd6178154ba86beb93f91f4136d7ee
Lucas Oliveira <lucasinfobarretos@gmail.com>
2022-06-02 09:54:16 -0300

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for host in $(cat /content/b3.txt);do
-echo "Realizando Analise do Ativo:"$host
-python3 script.py $host | tee final.txt
-done
-sort final.txt >> resultado.txt
-echo "Processo Concluido! Arquivo salvo como resultado.txt"
-rm final.txt
